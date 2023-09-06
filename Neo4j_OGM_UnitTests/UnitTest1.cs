51ed044df0eff8222420fc3b4e85d184304a34f0
Omar <omar.jabloun@insat.u-carthage.tn>
2023-09-07 00:30:27 +0100

    initialize the project

---
+++ Neo4j_OGM_UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Neo4j_OGM_UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

623e9a101d462d543247e221cdaf9766ec644130
Omar <omar.jabloun@insat.u-carthage.tn>
2023-09-07 00:31:00 +0100

    Add attributes

---
+++ Neo4j_OGM/Attributes/DbLabelNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace TMC.Domain.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class DbLabelNameAttribute : Attribute
+{
+    public DbLabelNameAttribute(string labelName)
+    {
+        LabelName = labelName;
+    }
+
+    /// <summary>
+    ///     The label name in neo4j db
+    /// </summary>
+    public string LabelName { get; }
+}
---
+++ Neo4j_OGM/Attributes/DbObjectNameAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace TMC.Domain.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class DbObjectNameAttribute : Attribute
+{
+    public DbObjectNameAttribute(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}

ee752367309e1ef389fe5695e23fbad777963dfb
Omar <omar.jabloun@insat.u-carthage.tn>
2023-09-07 00:31:16 +0100

    Add the executor

---
+++ Neo4j_OGM/Executor/INeo4jQueryExecutor.cs
@@ -0,0 +1,21 @@
+﻿using Neo4j.Driver;
+using TMC.Domain.Entities;
+using TMC.Infrastructure.Pagination;
+
+namespace TMC.Infrastructure.Neo4j;
+
+public interface INeo4jQueryExecutor : IDisposable, IAsyncDisposable
+{
+    Task<T> ExecuteWriteAsync<T>(Query query) where T : EntityBase<T>;
+    Task ExecuteWriteAsync(Query query);
+    Task<T> ExecuteInsideTransactionAsync<T>(IAsyncTransaction tx, Query query) where T : EntityBase<T>;
+    Task<T> ExecuteInsideTransactionPrimitiveAsync<T>(IAsyncTransaction tx, Query query);
+    Task ExecuteInsideTransactionAsync(IAsyncTransaction tx, Query query);
+    Task<IEnumerable<T>> ExecuteWriteAsyncToList<T>(Query query) where T : EntityBase<T>;
+    Task<T?> ExecuteReadAsync<T>(Query query) where T : EntityBase<T>;
+    Task<Pagination<T>> ExecuteReadPaginationAsync<T>(Query query) where T : EntityBase<T>;
+    Task<IEnumerable<T>> ExecuteReadAsyncToList<T>(Query query) where T : EntityBase<T>;
+    Task<long> ExecuteReadNumberAsync(Query query);
+    Task ExecuteTransactionAsync(Func<IAsyncTransaction, Task> transactionWork);
+    Task<TResult> ExecuteTransactionAsync<TResult>(Func<IAsyncTransaction, Task<TResult>> transactionWork);
+}
---
+++ Neo4j_OGM/Executor/Neo4jQueryExecutor.cs
@@ -0,0 +1,185 @@
+﻿using Neo4j.Driver;
+using TMC.Domain.Entities;
+using TMC.Domain.Exceptions;
+using TMC.Infrastructure.Data.RecordToObject;
+using TMC.Infrastructure.Pagination;
+
+namespace TMC.Infrastructure.Neo4j;
+
+public class Neo4jQueryExecutor : INeo4jQueryExecutor
+{
+    private readonly IDriver _driver;
+    private IAsyncSession? _session;
+
+    public Neo4jQueryExecutor(IDriver driver)
+    {
+        _driver = driver;
+    }
+
+
+    public async Task<T> ExecuteWriteAsync<T>(Query query) where T : EntityBase<T>
+    {
+        var session = await GetSession();
+        var result = await session.ExecuteWriteAsync(async tx =>
+        {
+            var cursor = await tx.RunAsync(query);
+            if (await cursor.FetchAsync())
+                return MapEntity.Map<T>(cursor.Current);
+            throw new NotFoundNeo4jException(" Not Found");
+        });
+        return result;
+    }
+
+    public async Task ExecuteWriteAsync(Query query)
+    {
+        var session = await GetSession();
+        await session.ExecuteWriteAsync(async tx =>
+        {
+            var result = await tx.RunAsync(query);
+            if (!await result.FetchAsync())
+                throw new QueryNotExecutedException("Query Not Executed");
+        });
+    }
+
+    public async Task<T> ExecuteInsideTransactionAsync<T>(IAsyncTransaction tx, Query query) where T : EntityBase<T>
+    {
+        var cursor = await tx.RunAsync(query);
+        if (await cursor.FetchAsync())
+            return MapEntity.Map<T>(cursor.Current);
+        throw new NotFoundNeo4jException(" Not Found");
+    }
+
+    public async Task<T> ExecuteInsideTransactionPrimitiveAsync<T>(IAsyncTransaction tx, Query query)
+    {
+        var cursor = await tx.RunAsync(query);
+        if (await cursor.FetchAsync())
+            return MapEntity.MapPrimitive<T>(cursor.Current);
+        throw new NotFoundNeo4jException(" Not Found");
+    }
+
+    public async Task ExecuteInsideTransactionAsync(IAsyncTransaction tx, Query query)
+    {
+        var result = await tx.RunAsync(query);
+        if (!await result.FetchAsync())
+            throw new QueryNotExecutedException("Query Not Executed");
+    }
+
+    public async Task<IEnumerable<T>> ExecuteWriteAsyncToList<T>(Query query) where T : EntityBase<T>
+    {
+        var session = await GetSession();
+        var result = await session.ExecuteWriteAsync(async tx =>
+        {
+            var cursors = await tx.RunAsync(query);
+            var records = await cursors.ToListAsync();
+            if (records.Count == 0)
+                throw new NotFoundNeo4jException(" Not Found");
+            return MapEntity.Map<T>(records);
+        });
+        return result;
+    }
+
+    public async Task<T?> ExecuteReadAsync<T>(Query query) where T : EntityBase<T>
+    {
+        var session = await GetSession();
+        var result = await session.ExecuteReadAsync(async tx =>
+        {
+            var cursor = await tx.RunAsync(query);
+            if (await cursor.FetchAsync())
+                return MapEntity.Map<T>(cursor.Current);
+            return null;
+        });
+        return result;
+    }
+
+    public async Task<Pagination<T>> ExecuteReadPaginationAsync<T>(Query query) where T : EntityBase<T>
+    {
+        var session = await GetSession();
+
+        var result = await session.ExecuteReadAsync(async tx =>
+        {
+            var cursor = await tx.RunAsync(query);
+            if (!await cursor.FetchAsync()) return new Pagination<T>();
+            ;
+            var record = cursor.Current;
+            return MapEntity.MapPagination<T>(record);
+        });
+        return result;
+    }
+
+
+    public async Task<IEnumerable<T>> ExecuteReadAsyncToList<T>(Query query) where T : EntityBase<T>
+    {
+        var session = await GetSession();
+
+        var result = await session.ExecuteReadAsync(async tx =>
+        {
+            var cursors = await tx.RunAsync(query);
+            var records = await cursors.ToListAsync();
+            return records.Count == 0 ? new List<T>() : MapEntity.Map<T>(records);
+        });
+        return result;
+    }
+
+    public async Task<long> ExecuteReadNumberAsync(Query query)
+    {
+        var session = await GetSession();
+        var result = await session.ExecuteReadAsync(async tx =>
+        {
+            var cursor = await tx.RunAsync(query);
+            if (!await cursor.FetchAsync())
+                throw new NotFoundNeo4jException("Number Not Found");
+            var x = cursor.Current[0];
+            return x.As<long>();
+        });
+        return result;
+    }
+
+    public async Task ExecuteTransactionAsync(Func<IAsyncTransaction, Task> transactionWork)
+    {
+        var session = await GetSession();
+        var tx = await session.BeginTransactionAsync();
+        try
+        {
+            await transactionWork(tx);
+            await tx.CommitAsync();
+        }
+        catch (Exception)
+        {
+            await tx.RollbackAsync();
+            throw;
+        }
+    }
+
+    public async Task<TResult> ExecuteTransactionAsync<TResult>(Func<IAsyncTransaction, Task<TResult>> transactionWork)
+    {
+        var session = await GetSession();
+        var tx = await session.BeginTransactionAsync();
+        try
+        {
+            var result = await transactionWork(tx);
+            await tx.CommitAsync();
+            return result;
+        }
+        catch (Exception)
+        {
+            await tx.RollbackAsync();
+            throw;
+        }
+    }
+
+    public void Dispose()
+    {
+        _session?.Dispose();
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        _session?.DisposeAsync();
+        return ValueTask.CompletedTask;
+    }
+
+    private Task<IAsyncSession> GetSession()
+    {
+        return Task.FromResult(_session ??= _driver.AsyncSession());
+    }
+}

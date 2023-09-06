2ab584595fce269b941b54beb37c90af1aa25666
Omar <omar.jabloun@insat.u-carthage.tn>
2023-09-07 00:31:25 +0100

    add query builder

---
+++ Neo4j_OGM/QueryBuilder/Neo4jQuery.cs
@@ -0,0 +1,55 @@
+﻿using Neo4j.Driver;
+
+namespace TMC.Infrastructure.Neo4j.QueryBuilder;
+
+public class Neo4jQuery : Query
+{
+    public Neo4jQuery(string text) : base(text)
+    {
+        Text = text;
+    }
+
+    public Neo4jQuery(string text, object parameters) : base(text, parameters)
+    {
+        Text = text;
+    }
+
+    public Neo4jQuery(string text, IDictionary<string, object> parameters) : base(text, parameters)
+    {
+        Text = text;
+    }
+
+    public string Text { get; set; }
+
+    public Neo4jQuery MATCH(string match)
+    {
+        Text += $" MATCH {match}";
+        return this;
+    }
+
+    public Neo4jQuery WHERE(string where)
+    {
+        Text += $" WHERE {where}";
+        return this;
+    }
+
+    public Neo4jQuery RETURN(string @return)
+    {
+        Text += $" RETURN {@return}";
+        return this;
+    }
+
+    public Neo4jQuery MatchByCompany(string relationName, long companyId)
+    {
+        Text.TrimStart();
+        if (Text.Substring(0, 5) != "MATCH")
+            throw new Exception("First query must be MATCH");
+        Text.Remove(0, 5);
+        Text.TrimStart();
+        Text += $"(company)-[:{relationName}]->";
+        Parameters.Add(new KeyValuePair<string, object>("companyId", companyId));
+        Text += "MATCH (company:Company) WHERE Id(company) = $companyId MATCH";
+
+        return this;
+    }
+}
